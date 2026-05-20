1d4e8e35c97779d23518fb71a59a238856ec5665
Gunisha Modi <gunishamodi2@gmail.com>
2026-05-21 04:19:10 +0530

    Update cartController.js

--- LookBook(BEE)/Back-end/controllers/cartController.js
+++ LookBook(BEE)/Back-end/controllers/cartController.js
@@ -1,93 +1,52 @@
-const fs = require("fs");
-const path = require("path");
+const Cart = require("../models/cart");
 
-const cartPath = path.join(__dirname, "../data/cart.json");
-
-// Get all cart items
-exports.getCart = (req, res) => {
-    fs.readFile(cartPath, "utf8", (err, data) => {
-        if (err) {
-            return res.status(500).json({ message: "Error reading cart" });
-        }
-        const cart = JSON.parse(data || "[]");
-        res.json(cart);
-    });
+// GET user's cart
+exports.getCart = async (req, res) => {
+  const userId = req.user.id;
+  let cart = await Cart.findOne({ user: userId });
+  if (!cart) {
+    cart = await Cart.create({ user: userId, items: [] });
+  }
+  res.json(cart.items);
 };
 
-// Add item to cart
-exports.addToCart = (req, res) => {
-    const newItem = req.body;
-    fs.readFile(cartPath, "utf8", (err, data) => {
-        let cart = [];
-        if (!err && data) {
-            cart = JSON.parse(data);
-        }
-        // check if item already exists
-        const existingItem = cart.find(
-            item => item.name === newItem.name && item.price === newItem.price
-        );
-        if (existingItem) {
-            // increase quantity
-            existingItem.quantity = (existingItem.quantity || 1) + 1;
-        } else {
-            // add new item
-            newItem.id = Date.now();
-            newItem.quantity = 1;
-            cart.push(newItem);
-        }
-        fs.writeFile(cartPath, JSON.stringify(cart, null, 2), err => {
-            if (err) {
-                return res.status(500).json({ message: "Error saving cart item" });
-            }
-            res.json({ message: "Cart updated", cart });
-        });
-    });
+// ADD item
+exports.addToCart = async (req, res) => {
+  const userId = req.user.id;
+  const { name, price, image } = req.body;
+  let cart = await Cart.findOne({ user: userId });
+  if (!cart) {
+    cart = await Cart.create({ user: userId, items: [] });
+  }
+  const existingItem = cart.items.find(
+    item => item.name === name && item.price === price
+  );
+  if (existingItem) {
+    existingItem.quantity += 1;
+  } else {
+    cart.items.push({ name, price, image, quantity: 1 });
+  }
+  await cart.save();
+  res.json(cart.items);
 };
 
-// Remove item
-exports.removeFromCart = (req, res) => {
-    const id = parseInt(req.params.id);
-    fs.readFile(cartPath, "utf8", (err, data) => {
-        let cart = JSON.parse(data || "[]");
-        cart = cart.filter(item => item.id !== id);
-        fs.writeFile(cartPath, JSON.stringify(cart, null, 2), (err) => {
-            if (err) {
-                return res.status(500).json({ message: "Error removing item" });
-            }
-            res.json({ message: "Item removed" });
-        });
-    });
+// UPDATE quantity
+exports.updateQuantity = async (req, res) => {
+  const userId = req.user.id;
+  const { quantity } = req.body;
+  const cart = await Cart.findOne({ user: userId });
+  const item = cart.items.id(req.params.id);
+  if (!item) return res.status(404).json({ message: "Item not found" });
+  item.quantity = quantity;
+  await cart.save();
+  res.json(cart.items);
 };
 
-// Clear entire cart
-exports.clearCart = (req, res) => {
-    fs.writeFile(cartPath, JSON.stringify([], null, 2), (err) => {
-        if (err) {
-            return res.status(500).json({ message: "Error clearing cart" });
-        }
-        res.json({ message: "Cart cleared" });
-    });
-};
-
-// Update item quantity
-exports.updateQuantity = (req, res) => {
-    const id = parseInt(req.params.id);
-    const { quantity } = req.body;
-    fs.readFile(cartPath, "utf8", (err, data) => {
-        if (err) {
-            return res.status(500).json({ message: "Error reading cart" });
-        }
-        let cart = JSON.parse(data || "[]");
-        const item = cart.find(i => i.id === id);
-        if (!item) {
-            return res.status(404).json({ message: "Item not found" });
-        }
-        item.quantity = quantity;
-        fs.writeFile(cartPath, JSON.stringify(cart, null, 2), err => {
-            if (err) {
-                return res.status(500).json({ message: "Error updating quantity" });
-            }
-            res.json({ message: "Quantity updated", cart });
-        });
-    });
+// DELETE item
+exports.removeFromCart = async (req, res) => {
+  const userId = req.user.id;
+  const cart = await Cart.findOne({ user: userId });
+  cart.items = cart.items.filter(item => item._id != req.params.id);
+  await cart.save();
+  res.json(cart.items);
 };
